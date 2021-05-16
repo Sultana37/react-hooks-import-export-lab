2ad48796535592249d27dc765a8cfae4d4f575f7
Esrat <imranzaman.318@gmail.com>
2021-05-16 15:52:15 -0400

    done

--- src/__tests__/About.test.js
+++ src/__tests__/About.test.js
@@ -1,7 +1,20 @@
 import "@testing-library/jest-dom";
 import { render } from "@testing-library/react";
 import About from "../components/About";
+import React from "react";
+import { image } from "../data/user";
 
+function About() {
+  return (
+    <div id="about">
+      <h2>About Me</h2>
+      <p>I Made This</p>
+      <img src={image} alt="I made this" />
+    </div>
+  );
+}
+
+export default About;
 test("it is exported as a default export", () => {
   try {
     expect(() => render(<About />)).not.toThrow();
--- src/__tests__/Home.test.js
+++ src/__tests__/Home.test.js
@@ -2,6 +2,22 @@ import "@testing-library/jest-dom";
 import { render } from "@testing-library/react";
 import Home from "../components/Home";
 
+  
+import React from "react";
+import { username, city } from "../data/user";
+
+function Home() {
+  return (
+    <div id="home">
+      <h1>
+        {username} is a Web Developer from {city}
+      </h1>
+    </div>
+  );
+}
+
+export default Home;
+
 test("it is exported as a default export", () => {
   try {
     expect(() => render(<Home />)).not.toThrow();
